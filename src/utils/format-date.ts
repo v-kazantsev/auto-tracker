d530dc1b65e20f9ba5bf5b8196163a61eeee3bd4
v-kazantsev <vvs201@mail.ru>
2023-12-25 11:15:36 +0300

    add options to date formatter

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,5 +1,7 @@
-export const dateFromIsoString = (IsoString: string) => {
+const defaultOptions:  Intl.DateTimeFormatOptions =  { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric'};
+
+export const dateFromIsoString = (IsoString: string, options: Intl.DateTimeFormatOptions | undefined = defaultOptions) => {
   if (!IsoString) return '';
   const date = new Date(IsoString);
-  return new Intl.DateTimeFormat("ru-RU").format(date);
+  return new Intl.DateTimeFormat("ru-RU", options).format(date);
 };
