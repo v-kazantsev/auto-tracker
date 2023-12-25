a05b2ff201baad60cb13d27f619f1500a3347b35
v-kazantsev <vvs201@mail.ru>
2023-12-25 08:41:43 +0300

    add memo

--- src/modules/devices/device-list-view.tsx
+++ src/modules/devices/device-list-view.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { Box, IconButton, Tooltip } from '@mui/material';
 import { Add } from '@mui/icons-material';
 import { DeviceList } from './device-list';
@@ -10,11 +11,11 @@ import { ActionPanel } from 'components';
 export const DeviceListView = () => {
   const { data, loading, error, filter, setFilter, modalOpen, handleCloseModal, handleOpenModal } = useDeviceListView();
 
-  const filteredData = () => {
+  const filteredData = useMemo(() => {
     if (!data || !Array.isArray(data)) return [];
     if (!filter) return data;
     return data.filter((item) => item?.id?.toString() === filter);
-  }
+  }, [filter, data]);
 
   if (error) return <UiCommonError />;
   
@@ -32,7 +33,7 @@ export const DeviceListView = () => {
             </Tooltip>
           ]}
         />
-        <DeviceList config={devicesTableConfig} tableData={filteredData()} />
+        <DeviceList config={devicesTableConfig} tableData={filteredData} />
         <UiDialog open={modalOpen} onClick={handleCloseModal} title='ОШИБКА'>
           {COMMON_NO_RIGHTS}
         </UiDialog>
